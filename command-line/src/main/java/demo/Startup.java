ad5e87d3e16b49240f1c8d7dbecabb992d22b1dc
marwensaid <marwensaidi1@gmail.com>
2015-11-18 14:26:23 +0100

    feat(cataOrange)  example auto configuration service with spring boot --MS

--- command-line/src/main/java/demo/Startup.java
+++ command-line/src/main/java/demo/Startup.java
@@ -3,7 +3,6 @@ package demo;
 import hello.HelloService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +16,6 @@ public class Startup implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-		helloService.sayHello();
+		helloService.myFirstHello();
 	}
 }
