27ab5af754eb95361c78586bb7714f4b4299e42f
marwensaid <marwensaidi1@gmail.com>
2015-11-18 14:29:13 +0100

    feat(cataOrange)  show webApp application--MS

--- my-webapp/src/test/java/demo/RunDemoApplicationTests.java
+++ my-webapp/src/test/java/demo/RunDemoApplicationTests.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
+@SpringApplicationConfiguration(classes = RunApplication.class)
 @WebAppConfiguration
-public class DemoApplicationTests {
+public class RunDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
